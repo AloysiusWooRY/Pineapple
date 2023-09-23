5f6b6629ba9a9d25a46449c662214d07fc451f5b
Alloy22 <aloysius22@hotmail.sg>
2023-09-23 18:53:40 +0800

    Add middleware to serve static assets (image)

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,12 @@ const cors = require("cors")
 const app = express()
 
 const accountRoutes = require('./routes/account')
+const organisationRoutes = require('./routes/organisation')
 
-// Middleware: for logging
 app.use(express.json())
 app.use(cors())
+
+// Middleware: for logging
 app.use((request, response, next) => {
     console.log(request.path, request.method)
     next()
@@ -17,6 +19,8 @@ app.use((request, response, next) => {
 
 // API Routes
 app.use('/api/account', accountRoutes)
+app.use('/api/organisation', organisationRoutes)
+app.use(express.static(__dirname + '/public'))
 
 // DB Conn
 mongoose.connect(process.env.MONGO_URI)
