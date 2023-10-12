390436b3f55ccbc3859daf6c574775af30c0b9cb
maximus-lee-678 <46864503+maximus-lee-678@users.noreply.github.com>
2023-10-12 14:43:55 +0800

    Added proxy middleware for cloud backend functionality

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -10,7 +10,7 @@ export const useLogin = () => {
     setIsLoading(true);
     setError(null);
 
-    const response = await fetch(`http://localhost:4000/api/account/login`, {
+    const response = await fetch(`/api/account/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
