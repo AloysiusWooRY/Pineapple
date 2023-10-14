0ab12ee1d8a5ea52ff3b246b50db0381037a8fa1
Alloy22 <aloysius22@hotmail.sg>
2023-10-14 23:29:16 +0800

    Add custom error handling

---
+++ backend/errors/customError.js
@@ -0,0 +1,66 @@
+const logger = require("../utils/logger")
+
+class ValidationError extends Error {
+    constructor(message, req) {
+        super(message)
+        this.statusCode = 400
+        this.name = this.constructor.name
+
+        logger.http(`${this.name} - ${message}`, { actor: "USER", req })
+        Error.captureStackTrace(this, this.constructor)
+    }
+}
+
+class MissingFieldError extends Error {
+    constructor(message, req) {
+        super(message)
+        this.statusCode = 400
+        this.name = this.constructor.name
+
+        logger.http(`${this.name} - ${message}`, { actor: "USER", req })
+        Error.captureStackTrace(this, this.constructor)
+    }
+}
+
+class DataNotFoundError extends Error {
+    constructor(message, req) {
+        super(message)
+        this.statusCode = 404
+        this.name = this.constructor.name
+
+        logger.http(`${this.name} - ${message}`, { actor: "USER", req })
+        Error.captureStackTrace(this, this.constructor)
+    }
+}
+
+class DuplicateRequestError extends Error {
+    constructor(message, req) {
+        super(message)
+        this.statusCode = 409
+        this.name = this.constructor.name
+
+        logger.http(`${this.name} - ${message}`, { actor: "USER", req })
+        Error.captureStackTrace(this, this.constructor)
+    }
+}
+
+class AuthenticationError extends Error {
+    constructor(message, req) {
+        super(message)
+        this.statusCode = 401
+        this.name = this.constructor.name
+
+        logger.warn(`${this.name} - ${message}`, { actor: "USER", req })
+        Error.captureStackTrace(this, this.constructor)
+    }
+}
+
+
+
+module.exports = {
+    ValidationError,
+    MissingFieldError,
+    DataNotFoundError,
+    DuplicateRequestError,
+    AuthenticationError
+} 
