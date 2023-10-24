ab9bfe82e66e559a42264ce800186e33ee38dc25
Alloy22 <aloysius22@hotmail.sg>
2023-10-25 00:23:23 +0800

    Update logger datatype to json

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -3,16 +3,26 @@ const { createLogger, transports, format } = require('winston')
 const { combine, timestamp, printf, simple, json } = format
 
 const fileLogFormat = printf(({ level, message, timestamp, actor, req }) => {
-    const formattedTimestamp = moment(timestamp).format('DD.MM HH:mm:ss');
+    const formattedTimestamp = moment(timestamp).format('DD.MM HH:mm:ss')
 
-    if (actor === "USER") {
-        const ip = req.headers?.["x-real-ip"] || req.ip;
-        const accountId = req.account ? req.account._id : "DEV";
-        const logMessage = `(${req.method} ${req.originalUrl}) ${message}`;
-        return `${formattedTimestamp} [${accountId}/${ip}] ${level.toUpperCase()}: ${logMessage}`;
+    const logObject = actor === "SERVER" ? {
+        timestamp: formattedTimestamp,
+        actor: "SERVER",
+        level: level.toUpperCase(),
+        message: message,
+    } : {
+        timestamp: formattedTimestamp,
+        actor: "SERVER",
+        account: req.account?._id ?? null,
+        ip: req.headers?.["x-real-ip"] || req.ip || null,
+        session: req.sessionID ?? null,
+        level: level.toUpperCase(),
+        message: message,
+        httpmethod: req.method,
+        httprequest: req.originalUrl
     }
 
-    return `${formattedTimestamp} [SERVER] ${level.toUpperCase()}: ${message}`;
+    return JSON.stringify(logObject);
 })
 
 const consoleLogFormat = printf(({ level, message }) => {
