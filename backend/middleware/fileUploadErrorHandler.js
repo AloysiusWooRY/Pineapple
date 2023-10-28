7206e09c501d243f27fa8fbf73ac0a582e1625de
Alloy22 <aloysius22@hotmail.sg>
2023-10-28 23:34:19 +0800

    Update file upload middleware

--- backend/middleware/fileUploadErrorHandler.js
+++ backend/middleware/fileUploadErrorHandler.js
@@ -1,5 +1,6 @@
 const { ValidationError } = require("../errors/customError")
 const logger = require("../utils/logger")
+const fs = require('fs')
 
 const fileUploadErrorHandler = async (err, req, res, next) => {
 
@@ -7,16 +8,21 @@ const fileUploadErrorHandler = async (err, req, res, next) => {
         try {
             if (err.code === 'LIMIT_UNEXPECTED_FILE')
                 throw new ValidationError(`Invalid file fields found: ${err.field}`, req)
+            else if (err.code === 'LIMIT_FILE_SIZE')
+                throw new ValidationError(`File too large`, req)
+            else if (err.code === 'LIMIT_FIELD_KEY')
+                throw new ValidationError(`File name too long`, req)
             else if (err.code === 'ENOENT')
                 throw new Error(`Error in folder creation: ${err.message}`)
             else if (req.errorCode === 'LIMIT_UNEXPECTED_FILE_TYPE')
-                throw new ValidationError(`Invalid file fields found: ${err.field}`, req)
+                throw new ValidationError(`Invalid file type found`, req)
             else
                 throw new Error(err.message)
         } catch (err) {
-            const { banner, poster } = req.info
-            if (banner) fs.unlinkSync(`uploads/banner/${banner.filename}`)
-            if (poster) fs.unlinkSync(`uploads/poster/${poster.filename}`)
+            const { banner, poster, attachment } = req.info
+            if (attachment && fs.existsSync(`uploads/attachment/${attachment.filename}`)) fs.unlinkSync(`uploads/attachment/${attachment.filename}`)
+            if (banner && fs.existsSync(`uploads/banner/${banner.filename}`)) fs.unlinkSync(`uploads/banner/${banner.filename}`)
+            if (poster && fs.existsSync(`uploads/poster/${poster.filename}`)) fs.unlinkSync(`uploads/poster/${poster.filename}`)
 
             if (err.statusCode === 400)
                 res.status(err.statusCode).json({ error: err.message })
