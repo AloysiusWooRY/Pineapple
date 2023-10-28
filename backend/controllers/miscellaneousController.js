f474637d3c4d89fa4627ca68390f42259e8f45ab
Alloy22 <aloysius22@hotmail.sg>
2023-10-28 17:24:13 +0800

    Add test email api

--- backend/controllers/miscellaneousController.js
+++ backend/controllers/miscellaneousController.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const fs = require('fs')
 const logger = require("../utils/logger")
+const sendEmail = require('../utils/sendEmail')
 
 // Ping pong
 const ping = async (req, res) => {
@@ -13,7 +14,18 @@ const generateCSRF = async (req, res) => {
     res.status(200).json({ csrfToken })
 }
 
+// Email test
+const emailTest = async (req, res) => {
+    const { email } = req.body
+    await sendEmail("PasswordReset", email, {
+        code: 696969,
+        name: "Jia Hao"
+    })
+    res.status(200).send()
+}
+
 module.exports = {
     ping,
-    generateCSRF
+    generateCSRF,
+    emailTest
 }
--- backend/routes/miscellaneous.js
+++ backend/routes/miscellaneous.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const {
     ping,
-    generateCSRF
+    generateCSRF,
+    emailTest
 } = require('../controllers/miscellaneousController')
 const rateLimit = require('../utils/rateLimiter')
 
 const router = express.Router()
 
-// POST: login account
+// GET: ping server
 router.get('/ping', rateLimit(), ping)
 
-// POST: create an account
+// GET: get csrf token
 router.get('/get-csrf-token', rateLimit(5, 100), generateCSRF)
+
+// GET: Send email
+router.get('/email', rateLimit(5, 100), emailTest)
 
 module.exports = router
