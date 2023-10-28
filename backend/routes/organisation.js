a50c41ab2eabfd06a9fb9e35eaeb3dd8e55b67f6
Alloy22 <aloysius22@hotmail.sg>
2023-10-28 23:54:13 +0800

    Add 3 organisation endpoints

--- backend/routes/organisation.js
+++ backend/routes/organisation.js
@@ -2,14 +2,18 @@ const express = require('express')
 const fs = require('fs')
 const { ValidationError } = require("../errors/customError")
 const {
-    applyOrganisation
+    applyOrganisation,
+    getAllOrganisation,
+    getAllCategories,
+    getOrganisationById
 } = require('../controllers/organisationController')
+const rateLimit = require('../utils/rateLimiter')
 const requireAuth = require('../middleware/requireAuth')
-const upload = require('../middleware/fileUpload')
+const fileUploadErrorHandler = require('../middleware/fileUploadErrorHandler')
 const logger = require("../utils/logger")
+const upload = require('../middleware/fileUpload')
 
 const router = express.Router()
-router.use(requireAuth(isAdmin = true))
 
 const uploadFields = upload.fields([
     { name: 'banner', maxCount: 1 },
@@ -17,31 +21,15 @@ const uploadFields = upload.fields([
 ])
 
 // POST: new org application
-router.post('/apply', async function (req, res, next) {
-    uploadFields(req, res, async function (err) {
-        if (err || req.errorCode) {
-            try {
-                if (err && err.code === 'LIMIT_UNEXPECTED_FILE')
-                    throw new ValidationError(`Invalid file fields found: ${err.field}`, req);
-                else if (err && err.code === 'ENOENT')
-                    throw new Error(`Error in folder creation: ${err.message}`);
-                else if (req.errorCode && req.errorCode === 'LIMIT_UNEXPECTED_FILE_TYPE')
-                    throw new ValidationError(`Invalid file type`, req);
-                else
-                    throw new Error(err.message);
-            } catch (err) {
-                if (err.statusCode === 400)
-                    res.status(err.statusCode).json({ error: err.message });
-                else {
-                    logger.error(err.message, { actor: "USER", req });
-                    res.status(500).json({ error: "Something went wrong, try again later" });
-                }
-            }
-        } else {
-            next()
-        }
-    });
-}, applyOrganisation);
+router.post('/apply', requireAuth(isAdmin = false), uploadFields, fileUploadErrorHandler, applyOrganisation);
 
+// GET: Category
+router.get('/categories', rateLimit(10, 50), requireAuth(isAdmin = false), getAllCategories)
+
+// GET: Organisation by category
+router.get('/all', rateLimit(10, 50), requireAuth(isAdmin = false), getAllOrganisation)
+
+// GET: Organisation by id
+router.get('/:id', rateLimit(10, 50), requireAuth(isAdmin = false), getOrganisationById)
 
 module.exports = router
