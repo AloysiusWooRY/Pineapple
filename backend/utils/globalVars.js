67ecd090014dbd2715d312ced303eed57be6ff6a
Alloy22 <aloysius22@hotmail.sg>
2023-10-28 23:32:58 +0800

    Add global var const

---
+++ backend/utils/globalVars.js
@@ -0,0 +1,15 @@
+const POST_FILTERS = ["new", "top"]
+const POST_CATEGORIES = ["all", "event", "discussion", "donation"]
+
+const ORG_CATEGORIES = ["health", "education", "environment", "humanitarian"]
+
+const MAX_TEXT_LEN = 256
+const MAX_LONG_TEXT_LEN = 2048
+
+module.exports = {
+    POST_FILTERS,
+    POST_CATEGORIES,
+    ORG_CATEGORIES,
+    MAX_TEXT_LEN,
+    MAX_LONG_TEXT_LEN
+}
