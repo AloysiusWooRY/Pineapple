d5fe79d1f36d31a5b9528b469d69548ab82fb8ad
Yap Jia Hao <y_jiahao@yahoo.com>
2023-10-28 23:58:21 +0800

    Create email.test.js

---
+++ backend/test/email.test.js
@@ -0,0 +1,20 @@
+const chai = require("chai");
+const { expect } = chai;
+
+describe("Connection Test for smtp, gmail", () => {
+    it("Successfully send Reset PW Email", async () => {
+    const email = "tosapap955@monutri.com"; // https://temp-mail.org/en/
+    const apiUrl = `http://localhost:4000/api/email?email=${encodeURIComponent(email)}`;
+       try {
+        const response = await fetch(apiUrl, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        expect(response.status).to.equal(200);
+      } catch (error) {
+        throw new Error(`HTTP request failed: ${error.message}`);
+      }
+    });
+});
