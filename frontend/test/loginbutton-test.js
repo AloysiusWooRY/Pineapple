cb7e050e455dcc90fc6b91a0610ef79ac4d72f53
chewweije <chewweijie2@gmail.com>
2023-10-30 00:18:31 +0800

    Update loginbutton-test.js

--- frontend/test/loginbutton-test.js
+++ frontend/test/loginbutton-test.js
@@ -1,12 +1,26 @@
-const { Builder, By, Key, until } = require('selenium-webdriver');
+const { Builder, By, until } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const { Options } = chrome;
+const { exec } = require('child_process');
 
-// Function to run the login test
+async function getCsrToken() {
+  return new Promise((resolve, reject) => {
+    exec('curl --location "http://localhost:4000/api/get-csrf-token" --header "Cookie: jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGYwNGU3NzQ5YmI5YWVkMzBhNWZmMmQiLCJpYXQiOjE2OTg1NTU3MDgsImV4cCI6MTY5ODY0MjEwOH0.NqIxJRicf-ItEcBxxCCpe0SJkaYKKj8wpWK80j_x_uY"', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error running curl: ${error}`);
+        reject(error);
+      } else {
+        const csrfToken = JSON.parse(stdout).csrfToken;
+        resolve(csrfToken);
+      }
+    });
+  });
+}
+
 async function runLoginTest() {
   const chromeOptions = new Options();
-  chromeOptions.addArguments('--headless'); // Run Chrome in headless mode (no GUI)
-  chromeOptions.addArguments('--disable-gpu'); // Disable GPU to prevent crashes in headless mode
+  chromeOptions.addArguments('--headless');
+  chromeOptions.addArguments('--disable-gpu');
 
   const driver = new Builder()
     .forBrowser('chrome')
@@ -14,41 +28,47 @@ async function runLoginTest() {
     .build();
 
   try {
-    // Navigate to the login page
-    await driver.get('http://localhost:4000/api/account/login'); // Replace with the actual URL
+    const csrfToken = await getCsrToken();
 
-    // Locate the email and password fields and the login button
-    const emailField = await driver.findElement(By.name('email')); // Adjust the selector as needed
-    const passwordField = await driver.findElement(By.name('password')); // Adjust the selector as needed
-    const loginButton = await driver.findElement(By.css('button[type="submit"]')); // Adjust the selector as needed
+    driver.get('http://localhost:4000/login');
 
-    // Enter email and password
-    await emailField.sendKeys('process.env.ADMIN_USER'); // Replace with your test email
-    await passwordField.sendKeys('process.env.ADMIN_PASS'); // Replace with your test password
+    // Use XPath or CSS selectors to locate the React-generated HTML elements
+    const emailField = await driver.findElement(By.xpath('//input[@placeholder="Enter Email Address"]'));
+    const passwordField = await driver.findElement(By.xpath('//input[@placeholder="Enter Password"]'));
+    const loginButton = await driver.findElement(By.xpath('//button[text()="Log In"]'));
 
-    // Click the login button
-    await loginButton.click();
+    // Include the CSRF token in the login request
+    const csrfHeader = { 'X-CSRF-TOKEN': csrfToken };
+    const headers = new Headers({
+      ...csrfHeader,
+      // Other headers, if needed
+    });
 
-    // Wait for an HTTP success code element to appear
-    await driver.wait(until.elementLocated(By.css('.http-success-code')), 10000);
+    await emailField.sendKeys(process.env.ADMIN_USER);
+    await passwordField.sendKeys(process.env.ADMIN_PASS);
 
-    // Get the HTTP success code element
-    const successCodeElement = await driver.findElement(By.css('.http-success-code'));
+    // Include the CSRF token in the request headers
+    await driver.executeScript(function (headers) {
+      fetch('/login', {
+        method: 'POST',
+        headers: headers,  // Include the CSRF token in the headers
+        body: JSON.stringify({
+          email: process.env.ADMIN_USER,
+          password: process.env.ADMIN_PASS,
+          // Include other login data as needed
+        }),
+      });
+    }, headers);
 
-    // Retrieve the text content of the success code element
-    const successCode = await successCodeElement.getText();
-
-    // Verify that the success code matches the expected value
-    if (successCode === '200 OK') {
-    console.log('Login successful. HTTP success code is 200 OK.');
-    } else {
-    console.error('Login failed. Unexpected HTTP success code:', successCode);
+    try {
+      await driver.wait(until.elementLocated(By.xpath('//input[@placeholder="Enter Recovery Code"]'), 10000));
+      console.log('Login button successful.');
+    } catch (error) {
+      console.error('Login button failed.');
     }
-
   } finally {
     await driver.quit();
   }
 }
 
-// Run the login test
 runLoginTest();
