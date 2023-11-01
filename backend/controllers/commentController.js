9731441c2173c7b670e60556abc4b666b90fe4ff
Alloy22 <aloysius22@hotmail.sg>
2023-11-01 22:08:40 +0800

    Update post/all to populate owner name

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -116,7 +116,7 @@ const deleteComment = async (req, res) => {
         await Like.deleteMany({ reply: { $in: replyIds } })
 
         logger.http(`Comment successfully deleted`, { actor: "USER", req })
-        res.status(200).send()
+        res.status(200).json({})
     } catch (err) {
         if (err.statusCode === 400 || err.statusCode === 404)
             res.status(err.statusCode).json({ error: err.message })
