4b91f546efb92380557affafa6d6677b2e81fbc5
chewweije <chewweijie2@gmail.com>
2023-11-03 23:00:01 +0800

    Update ui-test.js

--- frontend/test/ui-test.js
+++ frontend/test/ui-test.js
@@ -4,8 +4,10 @@ const chrome = require('selenium-webdriver/chrome');
 const { Options } = chrome;
 const { exec } = require('child_process');
 const { log } = require('console');
+const { tryParse } = require('selenium-webdriver/http');
 const email = testenv.TEST_USER_EMAIL;
 const password = testenv.TEST_USER_PASS;
+const authCode = testenv.AUTH_CODE;
 
 async function getCsrToken() {
   return new Promise((resolve, reject) => {
@@ -71,15 +73,47 @@ async function runLoginTest() {
     
     try {
       await driver.sleep(2000);
-      await driver.findElement(By.css('a[id="link-home"]'));
+      await driver.findElement(By.css('input[id="input-authenticator-code"]'));
       console.log('Login button successful.');
     } catch (error) {
       console.error('Login button failed.');
     }
-    //menu bar test
+
+    //auathentication button test
+    try {
+      const authInput = await driver.findElement(By.css('input[id="input-authenticator-code"]'));
+      const authButton = await driver.findElement(By.css('button[id="button-submit-authenticator-code"]'));
+      await authInput.sendKeys(authCode);
+      await authButton.click();
+      await driver.sleep(2000);
+      await driver.findElement(By.xpath('//h2[contains(text(),"Home")]'));
+      console.log('Authentication button successful.');
+    } catch (error) {
+      console.error('Authentication button failed.');
+    }
+
+    //home tabs test
+    try{
+      const homeDis = await driver.findElement(By.css('li[id="tab-post-types-option-discussion"]'))
+      await homeDis.click();
+      console.log('Discussion tab successful.');
+      const homeEve = await driver.findElement(By.css('li[id="tab-post-types-option-event"]'));
+      await homeEve.click();
+      console.log('Event tab successful.');
+      const homeDon = await driver.findElement(By.css('li[id="tab-post-types-option-donation"]'));
+      await homeDon.click();
+      console.log('Donation tab successful.');
+      const homeAll = await driver.findElement(By.css('li[id="tab-post-types-option-all"]'));
+      await homeAll.click();
+      console.log('Home All tab successful.');
+    } catch (error) {
+      console.error('Home tabs unsuccessful.')
+    }
+
+    //menu bar test: organization
     try{
       const menuOrg = await driver.findElement(By.css('a[id="link-organisation-home"]'));
-      await menuOrg.click()
+      await menuOrg.click();
       await driver.sleep(2000);
       await driver.findElement(By.xpath('//h2[contains(text(),"Organisations")]'));
       console.log('Organisation Menu button successful.');
@@ -87,6 +121,27 @@ async function runLoginTest() {
       console.error('Organisation Menu button unsuccessful.')
     }
 
+    //organisation tabs test
+    try{
+      const orgHea = await driver.findElement(By.css('li[id="tab-organisation-categories-option-health"]'))
+      await orgHea.click();
+      console.log('Health tab successful.');
+      const orgEdu = await driver.findElement(By.css('li[id="tab-organisation-categories-option-education"]'));
+      await orgEdu.click();
+      console.log('Education tab successful.');
+      const orgEnv = await driver.findElement(By.css('li[id="tab-organisation-categories-option-environment"]'));
+      await orgEnv.click();
+      console.log('Environment tab successful.');
+      const orgHum = await driver.findElement(By.css('li[id="tab-organisation-categories-option-humanitarian"]'))
+      await orgHum.click();
+      console.log('Humanitarian tab successful.');
+      const orgAll = await driver.findElement(By.css('li[id="tab-organisation-categories-option-all"]'));
+      await orgAll.click();
+      console.log('Organisation All tab successful.');
+    } catch (error) {
+      console.error('Organisation tabs unsuccessful.')
+    }
+    //menu bar test: profile
     try{
       const menuPro = await driver.findElement(By.css('a[id="link-profile"]'));
       await menuPro.click()
@@ -97,6 +152,27 @@ async function runLoginTest() {
       console.log('Profile buttons unsuccessful.')
     }
 
+    //home link test
+    try{
+      const home = await driver.findElement(By.css('a[id="link-home"]'));
+      await home.click()
+      await driver.sleep(2000);
+      await driver.findElement(By.xpath('//h2[contains(text(),"Home")]'));
+      console.log('Home link successful.');
+    } catch (error) {
+      console.log('Home link unsuccessful.')
+    }
+
+    //logout button test
+    try{
+      const logout = await driver.findElement(By.xpath('//span[contains(text(),"Logout")]'));
+      await logout.click()
+      await driver.sleep(2000);
+      await driver.findElement(By.css('input[placeholder="Enter Email Address"]'));
+      console.log('Logout button successful.');
+    } catch (error) {
+      console.log('Logout button unsuccessful.')
+    }
   } finally {
     await driver.quit();
   }
