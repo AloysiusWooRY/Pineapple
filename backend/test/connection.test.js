f59cd598147f398e784e919f53f97d8b8e80b31b
Yap Jia Hao <y_jiahao@yahoo.com>
2023-11-04 18:47:40 +0800

    Update connection.test.js

--- backend/test/connection.test.js
+++ backend/test/connection.test.js
@@ -4,9 +4,6 @@ const chai = require("chai");
 const { expect } = chai;
 let csrfToken = null;
 let cookie = null;
-const aEmail = process.env.TEST_ADMIN_EMAIL;
-const aPassword = process.env.TEST_ADMIN_PASS;
-const dev_secret = process.env.DEV_SECRET;
 const cookieFilter = (cookieString) => {
     const cookies = cookieString
       .split(", ")
@@ -23,7 +20,7 @@ const cookieFilter = (cookieString) => {
     return cookieObject;
   };
 
-describe("Connection Test where user are not supposed to be able to get a successful response from the server without logging in.", () => {
+describe("Connection Test it can reach the localhost server.", () => {
     it("Successfully Ping the server", async () => {
         try {
           const response = await fetch("http://localhost:4000/api/ping", {
@@ -53,46 +50,7 @@ describe("Connection Test where user are not supposed to be able to get a succes
         throw new Error(`HTTP request failed: ${error.message}`);
       }
     });
-    it("Unsuccessfully get all organisation information", async () => {
-        try {
-        const response = await fetch("http://localhost:4000/api/organisation/all", {
-            method: "POST",
-            headers: myHeaders
-        });
-        expect(response.status).to.equal(401);
-        } catch (error) {
-        throw new Error(`HTTP request failed: ${error.message}`);
-        }
-    });
-    it("Unsuccessfully get targeted organisation's posts", async () => {
-        try {
-        const response = await fetch("http://localhost:4000/api/post/all", {
-            method: "POST",
-            headers: myHeaders,
-            body: JSON.stringify({
-                organisation: process.env.TEST_ORGANISATION_1_ID,
-              }),
-        });
-        expect(response.status).to.equal(401);
-        } catch (error) {
-        throw new Error(`HTTP request failed: ${error.message}`);
-        }
-    });
-    it("Unsuccessfully get targeted organisation's comments", async () => {
-        try {
-        const response = await fetch("http://localhost:4000/api/comment/all", {
-            method: "POST",
-            headers: myHeaders,
-            body: JSON.stringify({
-                post: process.env.TEST_COMMENT_1_ID,
-              }),
-        });
-        expect(response.status).to.equal(401);
-        } catch (error) {
-        throw new Error(`HTTP request failed: ${error.message}`);
-        }
-    });
-  });
+});
 describe("Action Rate Limit (20). Flood the login request 20 times with wrong credentials, 21th to meet with a response of Too many Requests.", () => {
   for (let i = 1; i <= 21; i++) {
     let email;
