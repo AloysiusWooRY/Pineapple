b220e6e4e2f5ff784dd36ceec852b23b61af756e
Yap Jia Hao <y_jiahao@yahoo.com>
2023-11-05 22:44:33 +0800

    Update setup_deploy.sh

--- setup_deploy.sh
+++ setup_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd backend && npm install --omit=dev
-cd ../frontend && npm install --omit=dev && npm install -g serve && npm run prod-build
+cd ../frontend && npm install --omit=dev && npm install -g serve && npm install react-scripts && npm run prod-build && npm uninstall react-scripts
