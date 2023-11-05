8efb53d83bc3e66ecf47d27bd011b483bc64e020
Alloy22 <aloysius22@hotmail.sg>
2023-11-05 22:44:06 +0800

    Fix rate limiter to use ip in header

--- backend/utils/rateLimiter.js
+++ backend/utils/rateLimiter.js
@@ -6,6 +6,9 @@ const rateLimit = (window = 10, max = 20) => {
         windowMs: window * 60 * 1000, // 5 minutes
         max: max, // Requests per window
         message: "Too many attempts, please try again later",
+        keyGenerator: (req, res) => {
+            return req.headers["x-forwarded-for"];
+        },
         handler: (req, res) => {
             const rateLimitError = new RateLimitError("Too many attempts, please try again later", req)
             res.status(rateLimitError.statusCode).json({ error: rateLimitError.message })
