40edbf03552a1b988bee6c2459a4516c00f7a8ee
Alloy22 <aloysius22@hotmail.sg>
2023-11-05 12:36:52 +0800

    Add ip encryption for logging

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -2,25 +2,35 @@ const moment = require('moment')
 const { createLogger, transports, format } = require('winston')
 const { combine, timestamp, printf, simple, json } = format
 const DailyRotateFile = require('winston-daily-rotate-file')
+const CryptoJS = require("crypto-js")
 
 const fileLogFormat = printf(({ level, message, timestamp, actor, req }) => {
     const formattedTimestamp = moment(timestamp).format('DD.MM HH:mm:ss')
 
-    const logObject = actor === "SERVER" ? {
-        timestamp: formattedTimestamp,
-        actor: "SERVER",
-        level: level.toUpperCase(),
-        message: message,
-    } : {
-        timestamp: formattedTimestamp,
-        actor: "USER",
-        account: req.account?._id ?? null,
-        ip: req.headers?.["x-real-ip"] || req.ip || null,
-        session: req.sessionID ?? null,
-        level: level.toUpperCase(),
-        message: message,
-        httpmethod: req.method,
-        httprequest: req.originalUrl
+    let logObject = {}
+
+    if (actor === "SERVER") {
+        logObject = {
+            timestamp: formattedTimestamp,
+            actor: "SERVER",
+            level: level.toUpperCase(),
+            message: message
+        }
+    } else {
+        const ip = req.headers?.["x-real-ip"] || req.ip || null
+        const encryptIP = ip ? CryptoJS.AES.encrypt(ip, process.env.ENCRYPTION_SECRET).toString() : null
+
+        logObject = {
+            timestamp: formattedTimestamp,
+            actor: "USER",
+            account: req.account?._id ?? null,
+            ip: encryptIP,
+            session: req.sessionID ?? null,
+            level: level.toUpperCase(),
+            message: message,
+            httpmethod: req.method,
+            httprequest: req.originalUrl
+        }
     }
 
     return JSON.stringify(logObject)
